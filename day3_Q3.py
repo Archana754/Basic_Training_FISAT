7debed5250b6b5fabbe9d20ea58081727fdbeab1
ARCHANA S <151940165+Archana754@users.noreply.github.com>
2026-06-03 18:07:58 +0530

    Delete day3_Q3.py

--- day3_Q3.py
+++
@@ -1,8 +0,0 @@
-# https://leetcode.com/problems/single-number/
-
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        x1=0
-        for i in nums:
-            x1=x1^i
-        return x1
