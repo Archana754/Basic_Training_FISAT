25ab9311548411bccd2dd7c769a72cb606dadddf
ARCHANA S <151940165+Archana754@users.noreply.github.com>
2026-06-03 18:06:44 +0530

    Delete day2_Q2.py

--- day2_Q2.py
+++
@@ -1,8 +0,0 @@
-#print the elements in the lis which has occurred odd number of times
-
-l=input().split()
-res=[]
-for i in l:
-    if i not in res and l.count(i)%2!=0 :
-        res.append(i)
-print(res)
